1fdc5d1d4a3a54312f4ff1c942bbb2a703b6c296
nerdqueen <hannarocha000@gmail.com>
2021-08-25 21:22:43 -0300

    corrigindo

--- src/main/java/lista/alunos/Dados.java
+++ src/main/java/lista/alunos/Dados.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lista.alunos;
 
-/**
- *
- * @author Hanna
- */
+import java.util.ArrayList;
+
 public class Dados {
-    
+      ArrayList<Aluno> alunos =  new ArrayList <>();
+      
+      public void cadastrarAluno(Aluno alunos){
+        
+      }
 }
